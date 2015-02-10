6c2596eadef41b09a156e9ee0ef86ce7057f2794
leasy <sevenkplus@126.com>
2015-02-10 17:56:54 +0800

    learning virtual functions
    virtual method;
    abstract class can't be declared.

---
+++ L07VirtualMethod/People.cpp
@@ -0,0 +1,30 @@
+#include "People.h"
+using namespace std;
+
+People::People(){
+    age = 18;
+    sex = 0;
+}
+
+People::People(int age,int sex){
+    this->age = age;
+    this->sex = sex;
+}
+
+int People::getAge(){
+    return this->age;
+}
+int People::getSex(){
+    return this->sex;
+}
+
+void People::setAge(int age){
+    this->age = age;
+}
+void People::setSex(int sex){
+    this->sex = sex;
+}
+
+void People::sayHello(){
+    cout << "People Say:hello cpp" << endl;
+}
